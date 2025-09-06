e3a79324d3e19bebb248e5f6b6582b8af7aac9be
pentest-gg <zumtestenfuerdiscord@gmail.com>
2025-09-06 19:38:29 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+
+const basePath = import.meta.env.VITE_BASE_URL || "/";
 
 const queryClient = new QueryClient();
 
@@ -15,9 +16,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path={basePath} element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+          <Route path="*" element={<Navigate to={basePath} replace />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
