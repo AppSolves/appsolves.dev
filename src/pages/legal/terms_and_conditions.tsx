9eea0ce541cd77e2f2712f295657393ca4712647
AppSolves <appsolves@gmail.com>
2025-09-20 13:28:35 +0200

    .

--- src/pages/legal/terms_and_conditions.tsx
+++ src/pages/legal/terms_and_conditions.tsx
@@ -6,7 +6,7 @@ import ReactMarkdown from "react-markdown";
 export default function TermsAndConditions() {
     return <div className="dark min-h-screen bg-background">
         <Navigation />
-        <main className="prose prose-lg dark:prose-invert mx-auto pt-16 px-16 sm:px-0 mt-20 mb-16 transition-all duration-300">
+        <main className="prose prose-lg dark:prose-invert mx-auto pt-16 px-8 [@media(min-width:655px)]:px-0 mt-20 mb-16 transition-all duration-300">
             <ReactMarkdown>{tacText}</ReactMarkdown>
         </main>
         <Footer />
