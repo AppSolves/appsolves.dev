ce3cd999ca47cb97ef4d9bc52aabc6c0525a852b
AppSolves <appsolves@gmail.com>
2026-04-02 17:56:24 +0200

    Update Vite configuration and package dependencies; replace @vitejs/plugin-react-swc with @vitejs/plugin-react and clean up package.json and package-lock.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import path, { resolve } from "path";
 import { defineConfig, loadEnv } from "vite";
